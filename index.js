080da52f4105b07a93008a2d66d02b2f3d3ee856
webdevjscoder <cobianjuan19@gmail.com>
2020-07-29 12:12:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(array, func) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(func(array[i]));
+    }
+    return result;
+}
+
+function reduce(obj, cb, accumulator) {
+    const keys = Object.keys(obj)
+    console.log(keys)
+    let i = 1
+    if (accumulator) {
+      i = 0
+    } else {
+      accumulator = obj[keys[0]]
+    }
+    for (i; i < keys.length; i++) {
+      accumulator = cb(accumulator, obj[keys[i]])
+    }
+    return accumulator
+  }
